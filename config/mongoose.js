6824ec813c7ccf1142705152baae347b0d9c1741
hemanthcangit <134469207+hemanthcangit@users.noreply.github.com>
2023-08-12 22:14:16 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/db';
+const url = 'mongodb+srv://localhost/db';
 // mongoose.connect('url') 
 mongoose.connect(url);
 const db=mongoose.connection;
